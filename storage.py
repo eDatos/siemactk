c065006ca8cbbc6cf48f65b03b1f025d2d52919c
Sergio Delgado Quintero <sdelquin@gmail.com>
2021-05-20 11:24:40 +0100

    Get codelists and datasets inventory within downloading

--- storage.py
+++ storage.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+import pandas as pd
 from google.cloud import storage
 from yagdrive import GDrive
 
@@ -13,8 +14,18 @@ gcs = storage.Client()
 bucket = gcs.get_bucket('siemac')
 
 
-def download_codelist(filepath: str):
-    drive.get_by_id(settings.GDRIVE_CODELIST_ID, filepath)
+def download_dataref(
+    file_id=settings.GDRIVE_DATAREF_ID,
+    filepath=settings.DATAREF_FILENAME,
+    codelists_sheet=settings.DATAREF_CODELISTS_SHEET,
+    inventory_sheet=settings.DATAREF_INVENTORY_SHEET,
+    inventory_dataset_urls_column_name=settings.DATAREF_INVENTORY_DATASET_URLS_COLUMN_NAME,
+):
+    drive.get_by_id(file_id, filepath)
+    fh = Path(filepath)
+    codelists = pd.read_excel(fh, sheet_name=codelists_sheet)
+    inventory = pd.read_excel(fh, sheet_name=inventory_sheet)
+    return codelists, inventory[inventory_dataset_urls_column_name]
 
 
 def upload(file: Path):
