c72991eb558621ec850c292f2002880a136463fb
Sergio Delgado Quintero <sdelquin@gmail.com>
2021-05-20 12:07:21 +0100

    Get rid of unused code

--- scraping.py
+++ scraping.py
@@ -1,37 +1,10 @@
 import gzip
-import os
 import re
 from pathlib import Path
 
 import requests
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
 
 import settings
-
-
-def get_datasets_urls(target_url):
-    options = Options()
-    options.headless = True
-
-    driver = webdriver.Firefox(options=options)
-    driver.get(target_url)
-
-    element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, '#gepRegionIndicators .tipTable'))
-    )
-
-    items = element.find_elements_by_class_name('tipRow')
-    for item in items:
-        item_id = item.get_attribute('id')
-        dataset_code = re.match(r'tipRow_[\d-]+_(.*)', item_id).group(1)
-        dataset_table_url = os.path.join(
-            settings.BASE_DATASET_URL, dataset_code + '.tsv.gz'
-        )
-        yield dataset_table_url
 
 
 def download_dataset(dataset_url, target_folder=settings.DATASETS_DIR):
--- settings.py
+++ settings.py
@@ -2,16 +2,6 @@ import os
 
 from prettyconf import config
 
-TARGET_URL = config(
-    'TARGET_URL',
-    default='https://ec.europa.eu/eurostat/cache/RCI/myregion/'
-    '#?reg=ES70&ind=1-2_demo_r_d2jan',
-)
-BASE_DATASET_URL = config(
-    'BASE_DATASET_URL',
-    default='https://ec.europa.eu/eurostat/estat-navtree-portlet-prod/'
-    'BulkDownloadListing?file=data/',
-)
 DATASETS_DIR = config('DATASETS_DIR', default='data')
 TARGET_GEOCODES = config(
     'TARGET_GEOCODES', default='ES70,PT20,PT30,EU27_2020', cast=config.list
