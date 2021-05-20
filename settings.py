0007ef4f83295052053e4bd7a7b879facdd98ba5
Sergio Delgado Quintero <sdelquin@gmail.com>
2021-05-20 11:23:49 +0100

    Fix settings for data reference

--- settings.py
+++ settings.py
@@ -18,9 +18,13 @@ TARGET_GEOCODES = config(
 )
 RECODING_LANGUAGES = config('RECODING_LANGUAGES', default='ES,PT', cast=config.list)
 
-CODELIST_FILENAME = config('CODELIST_FILENAME', default='codelist.xlsx')
-GDRIVE_CODELIST_ID = config('GDRIVE_CODELIST_ID')
-CODELIST_RECODE_SHEET = config('CODELIST_RECODE_SHEET')
+DATAREF_FILENAME = config('DATAREF_FILENAME', default='dataref.xlsx')
+GDRIVE_DATAREF_ID = config('GDRIVE_DATAREF_ID')
+DATAREF_CODELISTS_SHEET = config('DATAREF_CODELISTS_SHEET', default='Codelists')
+DATAREF_INVENTORY_SHEET = config('DATAREF_INVENTORY_SHEET', default='Inventario Siemac')
+DATAREF_INVENTORY_DATASET_URLS_COLUMN_NAME = config(
+    'DATAREF_INVENTORY_DATASET_URLS_COLUMN_NAME', default='URL Dataset'
+)
 
 GDRIVE_API_CREDENTIALS = config('GDRIVE_API_CREDENTIALS', default='gdrive-credentials.json')
 GDRIVE_API_SECRETS = config('GDRIVE_API_SECRETS', default='gdrive-secrets.json')
